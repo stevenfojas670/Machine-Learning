36fb49e7b3a2aa6d68ff0be4af781f5683258e1a
Steven Fojas <84162024+stevenfojas670@users.noreply.github.com>
2024-01-29 17:47:51 -0800

    Imported numpy and pandas

---
+++ homework1/hw1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+'''
+Task 1: Visualize the MNIST data using PCA. Reduce the data dimension to two or three and plot the data of reduced
+dimension. Must plot all the data of ten groups
+    - Each data point should indicate its class label (using different colors or text)
+
+Task 2: Visualize the columns of K, M and N in the housing data using Boxplot.
+    - Box plot should show various density on the data at different values. Otherwise, it is just a box plot.
+
+Task 3: Visualize the column of A in the housing data using histogram.
+    - Histogram should show the distribution of the data in Column A.
+'''
+
